0bb454003b5cf0ef812454bb5e32e78f9b44fd5c
NetwCodeProjects <49507476+NetwCodeProjects@users.noreply.github.com>
2019-04-10 22:11:00 -0700

    Add files via upload
    Input length and get random password of uppercase, lowercase and numbers.

---
+++ Random pass gen.py
@@ -0,0 +1,25 @@
+# Importing random to generate  
+# random string sequence  
+import random  
+     
+# Importing string library function  
+import string
+
+# Create input for password length
+print("How many characters")
+quantity = input()
+     
+def rand_pass(size):  
+         
+    # Takes random choices from  
+    # ascii_letters and digits  
+    generate_pass = ''.join([random.choice( string.ascii_uppercase +
+                                            string.ascii_lowercase +
+                                            string.digits)  
+                                            for n in range(size)])  
+                             
+    return generate_pass  
+     
+# Driver Code   
+password = rand_pass(int(quantity)) 
+print(password)  
