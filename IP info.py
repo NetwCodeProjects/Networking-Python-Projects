ea262b5726a61d7fcb9c380c7c3901080142d70b
NetwCodeProjects <49507476+NetwCodeProjects@users.noreply.github.com>
2019-04-12 05:15:21 -0700

    Update IP info.py

--- IP info.py
+++ IP info.py
@@ -9,7 +9,6 @@ addr = input()
 dns = socket.gethostbyaddr(addr)
 # Define ip
 ip = IPAddress(addr)
-net = IPAddress(addr)
 # Print info
 print("IP version -", ip.version)
 print("private -", ip.is_private())
